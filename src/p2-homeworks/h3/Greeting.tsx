a275ca74e9d4dc41e1225c6939b4720b0f710e0d
KirillDanProg <kirill2898v@gmail.com>
2022-08-09 16:26:05 +0300

    add: error message added

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -14,14 +14,14 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = name ? "" : s.error // need to fix with (?:)
 
     return (
         <div>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
             <button onClick={addUser}>add</button>
             <span>{totalUsers}</span>
+            <div className={s.errorMessage}>{error ? error : ""}</div>
         </div>
     )
 }
