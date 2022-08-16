1af5cb73d7f48980ddc0a0f58e81ac73a417ba9a
KirillDanProg <kirill2898v@gmail.com>
2022-08-16 12:26:00 +0300

    add: empty pages created for HM6-HM12

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Junior() {
+    return (
+        <div>
+            Junior Page is not available for now
+            {/*<HM7></HM7>*/}
+            {/*<HM8></HM8>*/}
+            {/*<HM9></HM9>*/}
+            {/*<HM10></HM10>*/}
+            {/*<HM11></HM11>*/}
+        </div>
+    )
+}
---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function JuniorPlus() {
+    return (
+        <div>
+            JuniorPlus Page is not available for now
+            {/*<HM12></HM12>*/}
+        </div>
+    )
+}
