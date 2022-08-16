218ef14dedb82c85c17ff5672f29a8d371552913
KirillDanProg <kirill2898v@gmail.com>
2022-08-16 12:29:15 +0300

    HM 5 completed/gh-pages added/project deployed

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import s from './App.module.css'
-import HW1 from '../../../p2-homeworks/h1/HW1'
-import HW2 from "../../../p2-homeworks/h2/HW2";
-import HW3 from "../../../p2-homeworks/h3/HW3";
-import HW4 from "../../../p2-homeworks/h4/HW4";
-import HW5 from "../../../p2-homeworks/h5/HW5";
+
+import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5";
+import styles from "./App.module.css"
 
 
 
@@ -12,13 +10,12 @@ function App() {
 
     return (
         <div className={s.App}>
-            <div>react homeworks:</div>
-            <HW1/>
-            <HW2/>
-            <HW3/>
-            <HW4/>
+            <div className={styles.title}>react homeworks:</div>
+            {/*<HW1/>*/}
+            {/*<HW2/>*/}
+            {/*<HW3/>*/}
+            {/*<HW4/>*/}
             <HW5/>
-
         </div>
     )
 }
