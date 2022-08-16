da27b29bc8aa2e92f639c52c3c958444572ad880
KirillDanProg <kirill2898v@gmail.com>
2022-08-16 09:48:17 +0300

    className function for priority added

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -14,14 +14,27 @@ function Affair(props: AffairPropsType) {
         props.deleteAffairCallback(props.affair._id)
     }// need to fix
 
+    const setClassName = () => {
+        switch (props.affair.priority) {
+            case "low":
+                return styles.low
+            case "middle":
+                return styles.middle
+            case "high":
+                return styles.high
+        }
+    }
     return (
         <div className={styles.box}>
             <div className={styles.name}>{props.affair.name}</div>
+
             <SuperButton red onClick={deleteCallback}
-                         className={styles.delete}>delete</SuperButton>
-            <div className={`${styles.prior}  ${props.affair.priority === "low" ? styles.low :
-                props.affair.priority === "middle" ? styles.middle :
-                    styles.high}`}>{props.affair.priority}
+                         className={styles.delete}>
+                delete
+            </SuperButton>
+
+            <div className={`${styles.prior} ${setClassName()}`}>
+                {props.affair.priority}
             </div>
         </div>
     )
