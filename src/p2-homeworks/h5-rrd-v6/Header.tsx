b73ade1112995aaac43a4ceeb2455f137a7d53e5
KirillDanProg <kirill2898v@gmail.com>
2022-08-16 12:24:24 +0300

    add: header created links added

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+import {PATH} from "./Pages";
+import styles from "./Pages.module.css"
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [isShown, setIsShown] = React.useState(false)
 
-        </div>
+    const showMenuHandler = () => {
+        isShown ? setIsShown(false) : setIsShown(true)
+    }
+
+
+    return (
+        <>
+            <div className={`${styles.linksContainer} ${isShown ? styles.shown : ""}`}>
+
+                <Link to={PATH.PRE_JUNIOR} className={styles.linkItem}>PRE_JUNIOR</Link>
+
+                <Link to={PATH.JUNIOR} className={styles.linkItem}>JUNIOR</Link>
+
+                <Link to={PATH.JUNIOR_PLUS} className={styles.linkItem}>JUNIOR_PLUS</Link>
+                <SuperButton onClick={showMenuHandler} className={styles.btnMenu}>
+                    Menu
+                </SuperButton>
+
+            </div>
+        </>
     )
 }
 
