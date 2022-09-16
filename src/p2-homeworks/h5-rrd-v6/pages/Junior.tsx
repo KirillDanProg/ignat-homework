d9be4d241acd35c0f4f5515ce34f2720fa8b7619
KirillDanProg <kirill2898v@gmail.com>
2022-09-16 09:48:37 +0300

    HW7-HW9 added

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
+
+
 
 export function Junior() {
     return (
         <div>
-            Junior Page is not available for now
-            {/*<HM7></HM7>*/}
-            {/*<HM8></HM8>*/}
-            {/*<HM9></HM9>*/}
+            {/*Junior Page is not available for now*/}
+            <HW7/>
+            <HW8/>
+            <HW9/>
             {/*<HM10></HM10>*/}
             {/*<HM11></HM11>*/}
         </div>
