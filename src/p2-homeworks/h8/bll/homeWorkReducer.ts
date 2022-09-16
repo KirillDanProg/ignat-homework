f4790fdb356c3f2cfd2cd3e444b32eb227e29df3
KirillDanProg <kirill2898v@gmail.com>
2022-09-16 09:46:40 +0300

    HW8 reducer added "actions with users"

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,18 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: ActionsType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return action.payload === "up" ? [...state.sort((a, b) => a.name > b.name ? 1 : -1)] :
+                [...state.sort((a, b) => a.name < b.name ? 1 : -1)]
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(user => user.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+type ActionsType = {type: 'sort', payload: 'up'} | {type: 'sort', payload: 'down'} | {type: 'check', payload: 18}
